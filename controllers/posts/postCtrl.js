5defc3454dfa4f31e979613471f7b0e0d5f001fe
ryannguyen0303 <ryannguyen0303@gmail.com>
2021-12-07 23:32:02 +0700

    add post api

---
+++ controllers/posts/postCtrl.js
@@ -0,0 +1,15 @@
+const expressAsyncHandler = require("express-async-handler");
+const Post = require("../../model/post/Post");
+const validateMongodbId = require("../../utils/validateMongodbID");
+
+const createPostCtrl = expressAsyncHandler(async (req, res) => {
+  validateMongodbId(req.body.user);
+  try {
+    const post = await Post.create(req.body);
+    res.json(post);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+module.exports = { createPostCtrl };
---
+++ model/post/Post.js
@@ -0,0 +1,69 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Post title is required"],
+      trim: true,
+    },
+    //Created by only category
+    category: {
+      type: String,
+      required: [true, "Post category is required"],
+      default: "All",
+    },
+    isLike: {
+      type: Boolean,
+      default: false,
+    },
+    isDisLike: {
+      type: Boolean,
+      default: false,
+    },
+    numViews: {
+      type: Number,
+      default: 0,
+    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    disLikes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "Please Author is required"],
+    },
+    description: {
+      type: String,
+      required: [true, "Post description is required"],
+    },
+    image: {
+      type: String,
+      default:
+        "https://cdn.pixabay.com/photo/2021/12/03/21/56/lamps-6843881_960_720.jpg",
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    toObject: {
+      virtuals: true,
+    },
+    timestamps: true,
+  }
+);
+
+//compile
+const Post = mongoose.model("Post", postSchema);
+
+module.exports = Post;
