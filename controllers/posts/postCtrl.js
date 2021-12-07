823a0d55b712ebb2522ed9050224a4b512292f79
ryannguyen0303 <ryannguyen0303@gmail.com>
2021-12-07 23:44:19 +0700

    block user if they upload profane content

--- controllers/posts/postCtrl.js
+++ controllers/posts/postCtrl.js
@@ -1,9 +1,26 @@
 const expressAsyncHandler = require("express-async-handler");
+const Filter = require("bad-words");
 const Post = require("../../model/post/Post");
 const validateMongodbId = require("../../utils/validateMongodbID");
+const User = require("../../model/user/User");
 
 const createPostCtrl = expressAsyncHandler(async (req, res) => {
+  const { _id } = req.user;
   validateMongodbId(req.body.user);
+  //Check for bad words
+  const filter = new Filter();
+  const isProfane = filter.isProfane(req.body.title, req.body.description);
+  console.log(isProfane);
+  //Block user
+  if (isProfane) {
+    const user = await User.findByIdAndUpdate(_id, {
+      isBlocked: true,
+    });
+    throw new Error(
+      "Creating failed because it contains profane words and you have been blocked"
+    );
+  }
+
   try {
     const post = await Post.create(req.body);
     res.json(post);
