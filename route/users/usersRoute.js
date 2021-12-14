5c8b278456722c20260a8e7fab40ff4aa642acf9
ryannguyen0303 <ryannguyen0303@gmail.com>
2021-12-14 21:50:54 +0700

    user profile

--- route/users/usersRoute.js
+++ route/users/usersRoute.js
@@ -50,7 +50,7 @@ userRoutes.put("/unfollow", authMiddleware, unfollowUserCtrl);
 userRoutes.put("/block-user/:id", authMiddleware, blockUserCtrl);
 userRoutes.put("/unblock-user/:id", authMiddleware, unBlockUserCtrl);
 userRoutes.get("/profile/:id", authMiddleware, userProfileCtrl);
-userRoutes.put("/:id", authMiddleware, updateUserCtrl);
+userRoutes.put("/", authMiddleware, updateUserCtrl);
 userRoutes.delete("/:id", deleteUsersCtrl);
 userRoutes.get("/:id", fetchUserDetailsCtrl);
 
