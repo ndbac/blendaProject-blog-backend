7f959162500351be60507fa6eb8fd311bc0013b8
ryannguyen0303 <ryannguyen0303@gmail.com>
2021-12-21 16:03:46 +0700

    delete rs when uploading photo

--- middlewares/upload/photoUpload.js
+++ middlewares/upload/photoUpload.js
@@ -51,7 +51,7 @@ const postImgResize = async (req, res, next) => {
   req.file.filename = `user-${Date.now()}-${req.file.originalname}`;
 
   await sharp(req.file.buffer)
-    .resize(500, 500)
+    // .resize(500, 500)
     .toFormat("jpeg")
     .jpeg({ quality: 90 })
     .toFile(path.join(`public/images/posts/${req.file.filename}`));
